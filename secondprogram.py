fbdddde8dc520206174e7644f493f4f61cb1ce47
demonslayer-crypto <68649481+demonslayer-crypto@users.noreply.github.com>
2020-07-22 19:52:17 +0530

    Update secondprogram.py

--- secondprogram.py
+++ secondprogram.py
@@ -1,5 +1,4 @@
 filename = input("Input the Filename: ")
 f_extns = filename.split(".")
 print ("The extension of the file is : " + repr(f_extns[-1]))
-lis1 = [1,252,65423,"hello world"]
-print(list1(-1))
+
